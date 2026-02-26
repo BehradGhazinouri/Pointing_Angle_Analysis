aff55e73b9e2fb6043990b808bdada4e93a177f1
BehradGhazinouri <ghazinouribehrad@gmail.com>
2026-02-26 11:04:23 -0500

    Updated Folder Location

--- Main.py
+++ Main.py
@@ -1,16 +1,19 @@
 from beam_analyzer import BeamAnalyzer
 
-Folder_address82 = r'F:\Users\GENERAL\TEMP\Behrad\ASML\Hex2GRIN\Python\Pointing\82'
-Folder_address235 = r'F:\Users\GENERAL\TEMP\Behrad\ASML\Hex2GRIN\Python\Pointing\235'
+Folder_address82 = r'C:\Users\bghazinouri\Documents\Pointing\82'
+Folder_address235 = r'C:\Users\bghazinouri\Documents\Pointing\235'
 
 analyzer82=BeamAnalyzer(Folder_address82)
 analyzer235=BeamAnalyzer(Folder_address235)
 
 analyzer82.find_images()
+analyzer235.find_images()
 
 analyzer82.process_beams()
+analyzer235.process_beams()
 
-# analyzer82.save_visuals()
+analyzer82.save_visuals()
+analyzer235.save_visuals()
 
 
 
@@ -19,8 +22,15 @@ for res in analyzer82.results:
     print(f"file: {res['filename']}")
     print(f"\n Center:{res['center']}")
     print(f"\n Radius:{res['radius']}")
+for res in analyzer235.results:
+    print(f"file: {res['filename']}")
+    print(f"\n Center:{res['center']}")
+    print(f"\n Radius:{res['radius']}")
 
 
 analyzer82.analyze_groups(700) #value is something inbetween the radius in Z1 and Z2 positions
+analyzer235.analyze_groups(700)
+
 
 analyzer82.Pointing(700)
+analyzer235.Pointing(700)
