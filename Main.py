4c4e810fdbb4ad021a0391ff989af94d4af57caa
BehradGhazinouri <ghazinouribehrad@gmail.com>
2026-04-28 16:36:53 -0400

    Updated circle fit method

--- Main.py
+++ Main.py
@@ -1,19 +1,19 @@
 from beam_analyzer import BeamAnalyzer
 
-Folder_address82 = r'F:\Users\GENERAL\TEMP\Behrad\ASML\Hex2GRIN\acquisition\acquisituin_pupil\340004\POINTING\4\big'
-Folder_address235 = r'F:\Users\GENERAL\TEMP\Behrad\ASML\Hex2GRIN\acquisition\acquisituin_pupil\340004\POINTING\4\small'
+Folder_address82 = r'F:\Users\GENERAL\TEMP\Behrad\*\Hex2GRIN\acquisition\acquisituin_pupil\4-27-26 re-terminated\POINTING\340101-2\82-newjig'
+#Folder_address235 = r'F:\Users\GENERAL\TEMP\Behrad\*\Hex2GRIN\acquisition\acquisituin_pupil\4-27-26 re-terminated\POINTING\340101-2\235'
 
 analyzer82=BeamAnalyzer(Folder_address82)
-analyzer235=BeamAnalyzer(Folder_address235)
+#analyzer235=BeamAnalyzer(Folder_address235)
 
 analyzer82.find_images()
-analyzer235.find_images()
+#analyzer235.find_images()
 
 analyzer82.process_beams()
-analyzer235.process_beams()
+#analyzer235.process_beams()
 
 analyzer82.save_visuals()
-analyzer235.save_visuals()
+#analyzer235.save_visuals()
 
 
 
@@ -22,15 +22,18 @@ for res in analyzer82.results:
     print(f"file: {res['filename']}")
     print(f"\n Center:{res['center']}")
     print(f"\n Radius:{res['radius']}")
-for res in analyzer235.results:
+#for res in analyzer235.results:
     print(f"file: {res['filename']}")
     print(f"\n Center:{res['center']}")
     print(f"\n Radius:{res['radius']}")
 
 
 analyzer82.analyze_groups() 
-analyzer235.analyze_groups()
+#analyzer235.analyze_groups()
 
 
 analyzer82.Pointing()
-analyzer235.Pointing()
+#analyzer235.Pointing()
+
+analyzer82.save_to_csv("analysis_results.csv")
+#analyzer235.save_to_csv("analysis_results.csv")
